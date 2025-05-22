5b4c95c964f1c8ac57db906b49deafd8a5a173c6
mcmarius <23401453+mcmarius@users.noreply.github.com>
2025-05-22 21:39:24 +0000

    Update page.tsx
    Fix question count due to demo questions

--- app/instructions/page.tsx
+++ app/instructions/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
   const role = session?.user.role || 'user'
   let roleTerms = terms.filter((term) => term.domain == role.split("user-")[1])
   if(roleTerms.length == 0) {
-    roleTerms = terms
+    roleTerms = terms.filter((term) => term.domain != "demo")
   }
   const termIds = roleTerms.map((term) => term.id)
   const roleDefinitions = definitions.filter((definition) => termIds.includes(definition['term_id']))
